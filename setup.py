b26813ee428c62803f731a4851f828bd7f087844
Dave Voutila <voutilad@gmail.com>
2022-06-21 13:52:27 -0400

    bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dataflow-pyarrow-neo4j",
-    version="0.3.0",
+    version="0.4.0",
 
     url="https://github.com/neo4j-field/dataflow-flex-pyarrow-to-gds",
     maintainer="Dave Voutila",
